15421310b57a8c4d23aa5d38b5636cfcabb405db
ShahAli bogdadi <bogdadishahali@gmail.com>
2016-09-20 15:05:45 +0600

    updated font, responsive, design

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+    $('body').scrollspy({ target: '#phy' });
     $(window).scroll(function(event) {
         Scroll();
     });
 
     $('.navbar-collapse ul li a, .navbar-header a ').on('click', function() {
-        $('html, body').animate({scrollTop: $(this.hash).offset().top - 70}, 1000);
+        $('html, body').animate({scrollTop: $(this.hash).offset().top -0}, 1000);
         return false;
     });
+
+    /* ========== Background image height equal to the browser height.==========*/
+
+
+    if ($(window).width() > 1370){
+
+        $('.ws_images, .about, .gallery, .contact').css({ 'height': $(window).height() });
+        $(window).on('resize', function() {
+            $('.ws_images, .about, .gallery, .contact').css({ 'height': $(window).height() });
+        });
+    }
+
+    /*=========== scrollspy ===============*/
+
+
 });
+
+
+
